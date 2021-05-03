9a949a0ac9e1389c6af4606fd34fd9f311e1f704
Arvid Magnusson <arvidmagnusson97@gmail.com>
2021-05-03 11:32:10 +0200

    Modify user schema and add validation for login and register

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trime: true,
-      minlength: 3,
-    },
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trime: true,
+    minlength: 3,
   },
-  {
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
     timestamps: true,
-  }
-);
+    default: Date.now,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
