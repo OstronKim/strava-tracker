b778feb52865a6d8ede25a651344bb43d616767a
Arvid Magnusson <arvidmagnusson97@gmail.com>
2021-05-04 19:40:41 +0200

    add login component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Map from "./components/Map";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Navbar />
         <Route exact path="/" component={Landing} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </div>
     </Router>
   );
