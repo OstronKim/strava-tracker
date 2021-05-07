0d34404611cf06614a9baaf09f625034899ac79d
Arvid Magnusson <arvidmagnusson97@gmail.com>
2021-05-07 18:01:00 +0200

    Make dashboard a private page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./setAuthToken";
+
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-//import "./App.scss";
 
 import Map from "./components/Map";
 import Navbar from "./components/layout/Navbar";
@@ -12,6 +15,23 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
+
+//Keep user logged in if has valid token
+if (localStorage.jwtToken) {
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+
+  const decoded = jwt_decode(token);
+  //Set user and is Authenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  if (Date.now() >= decoded.exp * 1000) {
+    store.dispatch(logoutUser());
+    window.location.href = "./login";
+  }
+}
 
 function App() {
   return (
@@ -22,17 +42,13 @@ function App() {
           <Route exact path="/" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          <Switch>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          </Switch>
         </div>
       </Router>
     </Provider>
   );
 }
-
-/* <Router>
-  <div className="container">
-    <Route path="/" exact component={Map} />
-  </div>
-</Router>; */
 
 export default App;
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,30 @@
+// This PrivateRoute component is based on Tyler McGinnis
+// https://ui.dev/react-router-v4-protected-routes-authentication/
+
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      auth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" /> //Redirect back to login if not authenticated
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
