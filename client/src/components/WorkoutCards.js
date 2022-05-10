bee266123910e05a2a88db78d4bfe7c76ba82827
Arvid Magnusson <arvidmagnusson97@gmail.com>
2022-05-10 11:21:31 +0200

    Update design and layout for workout cards

--- client/src/components/WorkoutCards.js
+++ client/src/components/WorkoutCards.js
@@ -1,28 +1,66 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
 
-function Card(props) {
+function Wcard(props) {
   return (
-    <div className="card">
-      <div className="cardBody">
-        {/* <img src={props.img} /> */}
-        <h2 className="cardTitle">{props.title}</h2>
-        <ul className="cardDescription">
-          <li>Distance</li>
-          <li>Average pace</li>
-          <li>Elevation gain</li>
-        </ul>
-      </div>
-    </div>
+    <Card>
+      <Box
+        sx={{
+          maxWidth: 200,
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="100"
+          image="https://images.unsplash.com/photo-1610969524113-bae462bb3892?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          alt="running"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <List dense={true}>
+            <ListItem disablePadding>
+              <ListItemText primary="Distance"></ListItemText>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemText primary="Duration"></ListItemText>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemText primary="Pace"></ListItemText>
+            </ListItem>
+            <Divider />
+          </List>
+        </CardContent>
+        <CardActions style={{ justifyContent: "center" }}>
+          <Button variant="contained" size="small">
+            View route on map
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
   );
 }
-let titles = ["one", "two", "three"];
+let titles = ["one", "two", "three", "four", "five"];
 
 //Egentligen ta in de 5 senaste (eller färre om inte 5 finns) och hämta ut ur props
 function WorkoutCards(props) {
   return (
     <div className="cardRow">
       {titles.map(function (title) {
-        return <Card title={title} />;
+        return <Wcard key={title} title={title} />;
       })}
     </div>
   );
