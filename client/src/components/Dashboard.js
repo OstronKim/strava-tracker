735d8e3b0ec3ff55d7a8bc82096abc9c211e82ab
Arvid Magnusson <arvidmagnusson97@gmail.com>
2022-05-23 16:30:11 +0200

    move data fetching to parent component dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,12 +2,59 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../actions/authActions";
+import axios from "axios";
+import polyline from "@mapbox/polyline";
 import Map from "./Map";
 import WorkoutCards from "./WorkoutCards";
 
 import "./styles/style.scss";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.clientID = process.env.REACT_APP_STRAVA_CLIENTID;
+    this.clientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
+    this.refreshToken = process.env.REACT_APP_STRAVA_REFRESH_TOKEN;
+    this.auth_link = process.env.REACT_APP_STRAVA_AUTH_LINK;
+
+    this.state = {
+      activities: null, //Stores all activities. Borde inte vara en state-grej
+      currentActivity: null, //Den nuvarande valda aktiviteten
+      currentPolylines: [],
+    };
+  }
+
+  async componentDidMount() {
+    const stravaAuthResponse = await axios.all([
+      axios.post(
+        `${this.auth_link}?client_id=${this.clientID}&client_secret=${this.clientSecret}&refresh_token=${this.refreshToken}&grant_type=refresh_token`
+      ),
+    ]);
+    const respAccessToken = stravaAuthResponse[0].data.access_token;
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${respAccessToken}`;
+    console.log("Just a check to see how often strava api call is run");
+    fetch(activities_link)
+      .then((res) => res.json())
+      .then((data) => {
+        const polylines = [];
+        for (let i = 0; i < data.length; i += 1) {
+          const activity_polyline = data[i].map.summary_polyline;
+          // Skip polyline step if no route exists
+          if (activity_polyline != null) {
+            const activity_name = data[i].name;
+            const activity_elevation = data[i].total_elevation_gain;
+            polylines.push({
+              activityPositions: polyline.decode(activity_polyline),
+              activityName: activity_name,
+              activityElevation: activity_elevation,
+            });
+          }
+        }
+        this.setState({ currentPolylines: polylines, activities: data });
+      });
+  }
+
   onLogOutClick = (e) => {
     e.preventDefault();
     this.props.logoutUser();
@@ -23,7 +70,7 @@ class Dashboard extends Component {
         </h3>
         <button onClick={this.onLogOutClick}>Logout</button>
         <WorkoutCards />
-        <Map />
+        <Map polylines={this.state.currentPolylines} />
       </div>
     );
   }
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,77 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Popup, Polyline } from "react-leaflet";
-import axios from "axios";
-import polyline from "@mapbox/polyline";
 
-function Map() {
-  const [activities, setActivites] = useState([]);
+function Map(props) {
+  const [currentPolylines, setCurrentPolylines] = useState([]);
 
-  const clientID = process.env.REACT_APP_STRAVA_CLIENTID;
-  const clientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
-  const refreshToken = process.env.REACT_APP_STRAVA_REFRESH_TOKEN;
-  const auth_link = process.env.REACT_APP_STRAVA_AUTH_LINK;
-  const all_activities_link = process.env.REACT_APP_STRAVA_ALL_ACTIVITIES_LINK;
-  const accessToken = process.env.REACT_APP_STRAVA_ACCESS_TOKEN;
-
-  //UseEffect is a hook that runs at first render and also after every update
-  //TODO: Maybe not use useEffect function for this since strava limits the amount of requests. Consider using it in ComponentDidMount and a button to fetch it on demand.
-
+  // Update polylines when props.polylines changes
   useEffect(() => {
-    async function getActivites(res) {
-      const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}`;
-      fetch(activities_link)
-        .then((res) => res.json())
-        .then((data) => {
-          const polylines = [];
-          for (let i = 0; i < data.length; i += 1) {
-            const activity_polyline = data[i].map.summary_polyline;
-            // Skip polyline step if no route exists
-            if (activity_polyline != null) {
-              const activity_name = data[i].name;
-              const activity_elevation = data[i].total_elevation_gain;
-              polylines.push({
-                activityPositions: polyline.decode(activity_polyline),
-                activityName: activity_name,
-                activityElevation: activity_elevation,
-              });
-            }
-          }
-          console.log(polylines);
-          setActivites(polylines);
-        });
-    }
-    getActivites();
-
-    // async function fetchData() {
-    //   console.log("fetch data called");
-    //   const stravaAuthResponse = await axios.all([
-    //     //Get a fresh access token. Maybe not call it in useEffect function since it updates frequently.
-    //     axios.post(
-    //       `${auth_link}?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
-    //     ),
-    //   ]);
-    //   console.log(stravaAuthResponse[0].data.access_token);
-    //   //Use the fresh access token to get all activities
-    //   const stravaActivityResponse = await axios.get(
-    //     `${all_activities_link}?access_token=${stravaAuthResponse[0].data.access_token}`
-    //   );
-    //   //console.log(stravaActivityResponse);
-    //   const polylines = [];
-    //   for (let i = 0; i < stravaActivityResponse.data.length; i += 1) {
-    //     const activity_polyline =
-    //       stravaActivityResponse.data[i].map.summary_polyline;
-    //     const activity_name = stravaActivityResponse.data[i].name;
-    //     const activity_elevation =
-    //       stravaActivityResponse.data[i].total_elevation_gain;
-    //     polylines.push({
-    //       activityPositions: polyline.decode(activity_polyline),
-    //       activityName: activity_name,
-    //       activityElevation: activity_elevation,
-    //     });
-    //   }
-    //   setActivites(polylines);
-    // }
-  }, []); //[] to avoid useEffect infinite loop
+    setCurrentPolylines(props.polylines);
+  }, [props.polylines]);
 
   return (
     <div className="Map">
@@ -85,12 +21,12 @@ function Map() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           //https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}
         />
-        {activities.map((activity, i) => (
-          <Polyline key={i} positions={activity.activityPositions}>
+        {currentPolylines.map((polyline, i) => (
+          <Polyline key={i} positions={polyline.activityPositions}>
             <Popup>
               <div>
-                <h2>{"Name: " + activity.activityName}</h2>
-                <p>{"Total Elevation Gain: " + activity.activityElevation}</p>
+                <h2>{"Name: " + polyline.activityName}</h2>
+                <p>{"Total Elevation Gain: " + polyline.activityElevation}</p>
               </div>
             </Popup>
           </Polyline>
