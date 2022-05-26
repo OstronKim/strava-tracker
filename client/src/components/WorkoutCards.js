3a62a6f10cc9ba3073feb28aed796ab381143430
Arvid Magnusson <arvidmagnusson97@gmail.com>
2022-05-26 14:27:45 +0200

    show data on workoutcards

--- client/src/components/WorkoutCards.js
+++ client/src/components/WorkoutCards.js
@@ -11,53 +11,6 @@ import ListItem from "@mui/material/ListItem";
 import Divider from "@mui/material/Divider";
 import ListItemText from "@mui/material/ListItemText";
 
-function Wcard(props) {
-  return (
-    <Card className="card">
-      <Box
-        className="cardContent"
-        sx={{
-          maxWidth: 200,
-        }}
-      >
-        <CardMedia
-          component="img"
-          height="100"
-          image="https://images.unsplash.com/photo-1610969524113-bae462bb3892?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-          alt="running"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.title}
-          </Typography>
-          <List dense={true}>
-            <ListItem disablePadding>
-              <ListItemText primary="Distance"></ListItemText>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemText primary="Duration"></ListItemText>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemText primary="Pace"></ListItemText>
-            </ListItem>
-            <Divider />
-          </List>
-        </CardContent>
-        <CardActions style={{ justifyContent: "center" }}>
-          <Button variant="contained" size="small">
-            View route on map
-          </Button>
-        </CardActions>
-      </Box>
-    </Card>
-  );
-}
-let titles = ["one", "two", "three", "four", "five"];
-console.log();
-
-//Egentligen ta in de 5 senaste (eller färre om inte 5 finns) och hämta ut ur props
 function WorkoutCards(props) {
   const [latestActivities, setLatestActivities] = useState([]);
 
@@ -76,9 +29,80 @@ function WorkoutCards(props) {
   return (
     <div className="cardRow">
       {latestActivities.map(function (activity, i) {
-        return <Wcard key={i} title={activity.name} />;
+        return <Wcard key={i} info={activity} />;
       })}
     </div>
+  );
+}
+
+function Wcard(props) {
+  let dist, avgPace;
+  const convertDistance = (dist) => {
+    return (dist / 1000).toFixed(1);
+  };
+
+  const convertPace = (dist) => {
+    return ((props.info.average_speed / 1000) * 60).toFixed(2);
+  };
+
+  return (
+    <Card className="card">
+      <Box
+        className="cardContent"
+        sx={{
+          maxWidth: 200,
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="100"
+          image="https://images.unsplash.com/photo-1610969524113-bae462bb3892?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          alt="running"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.info.type}
+          </Typography>
+          <List dense={true}>
+            <ListItem disablePadding>
+              <ListItemText primary="Duration"></ListItemText>
+              <ListItemText
+                primary={`${props.info.elapsed_time} s`}
+              ></ListItemText>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemText primary="Distance"></ListItemText>
+              <ListItemText
+                primary={`${convertDistance(props.info.distance)} m`}
+              ></ListItemText>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemText primary="Pace"></ListItemText>
+              {/* <ListItemText
+                primary={`${this.props.info.average_speed} m`}
+              ></ListItemText> */}
+              <ListItemText
+                primary={`${props.info.average_speed.toFixed(1)} m/s`}
+              ></ListItemText>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemText primary="Elevation"></ListItemText>
+              <ListItemText
+                primary={`${props.info.total_elevation_gain} m`}
+              ></ListItemText>
+            </ListItem>
+          </List>
+        </CardContent>
+        <CardActions style={{ justifyContent: "center" }}>
+          <Button variant="contained" size="small">
+            View route on map
+          </Button>
+        </CardActions>
+      </Box>
+    </Card>
   );
 }
 
